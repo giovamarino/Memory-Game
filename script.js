15366565f9438ce3ce14bf2d74f3b5daaa3265e3
Giovanni Marino <giovanni-marino@lambdastudents.com>
2021-10-05 01:01:52 -0400

    made buttons unclickable when wrong answer selected

--- script.js
+++ script.js
@@ -3,12 +3,12 @@
 // random number generator
 let randomNumber = 0;
 
-// the first flash
-function flashButton() {
+// the first randomly generated flash + new randomly generated flashes
+// generates a new number, turns the box purple, then transparent, then pushes the number to the arr
+function newButton() {
   // random number generator
   randomNumber = Math.ceil(Math.random() * 16);
 
-  // 1st purple flash
   document.querySelector(`.btn-${randomNumber}`).style.cssText = `
   transition-delay: 0.25s;
   background-color: darkviolet;
@@ -22,6 +22,8 @@ function flashButton() {
   }, 250);
 
   randomNumberStorage.push(randomNumber);
+
+  //unrelated console logs
   console.log(randomNumberStorage);
   for (let index = 0; index < randomNumberStorage.length; index++) {
     console.log(`highlight ${randomNumberStorage.length} times`);
@@ -30,13 +32,15 @@ function flashButton() {
 
 // highlighting a button on refresh
 window.onload = function () {
+  // selects a button (1-16)
   for (let i = 1; i < 17; i++) {
     let buttonClass = `.btn-${i}`;
 
+    // Check to see if the randomly generated number matches the number of the button clicked
     document.querySelector(buttonClass).addEventListener(`click`, function () {
-      //Check to see if the random number that was generated matched the number of the button
+      // if user clicks the RIGHT box: turn box green, then transparent
       if (i === randomNumber) {
-        document.querySelector(`.btn-${i}`).style.cssText = `       
+        document.querySelector(`.btn-${i}`).style.cssText = `
           background-color: limegreen;
       `;
 
@@ -47,29 +51,36 @@ window.onload = function () {
           `;
         }, 50);
 
-        setTimeout(() => {
-          flashButton();
-        }, 300);
-      } else {
-        document.querySelector(`.btn-${i}`).style.cssText = `        
-          background-color: red;
-      `;
+        // highlight the squares
+        // for (let index = 0; index < randomNumberStorage.length; index++) {
+        //   // highlight the squares
+        // }
 
+        // makes a new button after changing box colors
         setTimeout(() => {
-          document.querySelector(`.btn-${i}`).style.cssText = `
-            transition-delay: 0.75s;
-            background-color: transparent;
-          `;
-        }, 750);
+          newButton();
+        }, 300);
+      }
+
+      // if user clicks the WRONG box: turn box red & disable clicking
+      else {
+        document.querySelector(`.btn-${i}`).style.backgroundColor = `red`;
+
+        // disables clicking
+        for (let i = 0; i < 16; i++) {
+          document.querySelectorAll(`.button`)[i].style.pointerEvents = `none`;
+        }
+
+        // setTimeout(() => {
+        //   document.querySelector(`.btn-${i}`).style.cssText = `
+        //     transition-delay: 0.75s;
+        //     background-color: transparent;
+        //   `;
+        // }, 750);
       }
     });
   }
-
-  flashButton();
+  newButton();
 };
 
 let randomNumberStorage = [];
-
-for (let index = 0; index < randomNumberStorage.length; index++) {
-  console.log(`highlight ${randomNumberStorage.length} times`);
-}
