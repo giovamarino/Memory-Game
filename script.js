a793a3f4be0e97fba149b7905f5e9ba756890171
Giovanni Marino <giovanni-marino@lambdastudents.com>
2021-10-14 16:46:08 -0400

    made the sequence work

--- script.js
+++ script.js
@@ -4,22 +4,15 @@
 let randomNumber = 0;
 let randomNumberStorage = [];
 
-// the first randomly generated flash + new randomly generated flashes
-// generates a new number, turns the box purple, then transparent, then pushes the number to the arr
+let indexOfButtonToHighlight = 0;
+let sequenceRepeatCurrentIndex = 0; // used as the index for randomNumberStorage
+let userCanPressButtons = false;
+
+/* the first randomly generated flash + new randomly generated flashes generates a new number, turns the box purple, then transparent, then pushes the number to the arr */
 function newButton() {
+  sequenceRepeatCurrentIndex = 0;
   // random number generator
-  randomNumber = Math.ceil(Math.random() * 16);
-
-  document.querySelector(`.btn-${randomNumber}`).style.cssText = `
-  background-color: darkviolet;
-`;
-
-  setTimeout(() => {
-    document.querySelector(`.btn-${randomNumber}`).style.cssText = `
-  transition-delay: 0.25s;
-  background-color: transparent;
-`;
-  }, 250);
+  let randomNumber = Math.ceil(Math.random() * 16);
 
   randomNumberStorage.push(randomNumber);
 
@@ -28,72 +21,106 @@ function newButton() {
   for (let index = 0; index < randomNumberStorage.length; index++) {
     console.log(`highlight ${randomNumberStorage.length} times`);
   }
+
+  flashButton();
+
+  // change score
+  document.querySelector(`.score`).innerHTML = randomNumberStorage.length - 1;
+
+  // change highscore
+  let score = Number(document.querySelector(`.score`).innerHTML);
+  let highScore = Number(document.querySelector(`.highScoreNumber`).innerHTML);
+
+  if (score > highScore) {
+    document.querySelector(`.highScoreNumber`).innerHTML =
+      document.querySelector(`.score`).innerHTML;
+  }
+}
+
+function flashButton() {
+  // Highlight each button from randomNumberStorage
+  let buttonClass2 = `.btn-${randomNumberStorage[indexOfButtonToHighlight]}`;
+
+  setTimeout(() => {
+    document.querySelector(buttonClass2).style.cssText = `
+    background-color: darkviolet;
+  `;
+
+    setTimeout(() => {
+      document.querySelector(buttonClass2).style.cssText = `
+    transition-delay: 0.05;
+    background-color: transparent;
+  `;
+    }, 250);
+
+    if (indexOfButtonToHighlight < randomNumberStorage.length - 1) {
+      indexOfButtonToHighlight++;
+
+      setTimeout(flashButton, 250);
+    } else {
+      userCanPressButtons = true;
+      indexOfButtonToHighlight = 0;
+    }
+  }, 250);
 }
 
 // highlighting a button on refresh
 window.onload = function () {
   // selects a button (1-16)
   for (let i = 1; i < 17; i++) {
-    let buttonClass = `.btn-${i}`;
-
     // Check to see if the randomly generated number matches the number of the button clicked
-    document.querySelector(buttonClass).addEventListener(`click`, function () {
+    document.querySelector(`.btn-${i}`).addEventListener(`click`, function () {
       // if user clicks the RIGHT box: turn box green, then transparent
-      if (i === randomNumber) {
-        document.querySelector(`.btn-${i}`).style.cssText = `
-          background-color: limegreen;
-      `;
-        setTimeout(() => {
+      if (userCanPressButtons) {
+        if (i === randomNumberStorage[sequenceRepeatCurrentIndex]) {
           document.querySelector(`.btn-${i}`).style.cssText = `
-            transition-delay: 0.05s;
-            background-color: transparent;
-          `;
-        }, 50);
+            background-color: limegreen;
+        `;
+          setTimeout(() => {
+            document.querySelector(`.btn-${i}`).style.cssText = `
+              transition-delay: 0.05s;
+              background-color: transparent;
+            `;
+          }, 50);
 
-        // let scoreText = document.querySelector(`.score`).innerHTML;
-        // let highScoreText =
-        //   document.querySelector(`.highScoreNumber`).innerHTML;
+          // change score
+          // document.querySelector(`.score`).innerHTML =
+          //   randomNumberStorage.length;
 
-        // change score
-        document.querySelector(`.score`).innerHTML = randomNumberStorage.length;
+          // // change highscore
+          // let score = Number(document.querySelector(`.score`).innerHTML);
+          // let highScore = Number(
+          //   document.querySelector(`.highScoreNumber`).innerHTML
+          // );
 
-        // change highscore
-        let score = Number(document.querySelector(`.score`).innerHTML);
-        let highScore = Number(
-          document.querySelector(`.highScoreNumber`).innerHTML
-        );
+          // if (score > highScore) {
+          //   document.querySelector(`.highScoreNumber`).innerHTML =
+          //     document.querySelector(`.score`).innerHTML;
+          // }
 
-        if (score > highScore) {
-          document.querySelector(`.highScoreNumber`).innerHTML =
-            document.querySelector(`.score`).innerHTML;
-          console.log(`hello`);
+          // makes a new button after changing box colors
+
+          if (sequenceRepeatCurrentIndex < randomNumberStorage.length - 1) {
+            sequenceRepeatCurrentIndex++;
+          } else {
+            setTimeout(() => {
+              userCanPressButtons = false;
+              newButton();
+            }, 300);
+          }
         }
 
-        // Highlight each button from randomNumberStorage
-        // for (let index = 0; index < randomNumberStorage.length; index++) {
-        //   let buttonClass2 = `.btn-${index}`;
-        //   setTimeout(() => {
-        //     document.querySelector(buttonClass2).style.cssText = `
-        //       background-color: limegreen;
-        //     `;
+        // if user clicks the WRONG box: turn box red & disable clicking
+        else {
+          // userCanPressButtons = false;
+          document.querySelector(`.btn-${i}`).style.backgroundColor = `red`;
 
-        //     console.log(randomNumberStorage[index]);
-        //     console.log(document.querySelector(`btn-${index}`));
-        //   }, 500);
-        // }
-        // makes a new button after changing box colors
-        setTimeout(() => {
-          newButton();
-        }, 300 /*5000*/);
-      }
-
-      // if user clicks the WRONG box: turn box red & disable clicking
-      else {
-        document.querySelector(`.btn-${i}`).style.backgroundColor = `red`;
-
-        // disables clicking
-        for (let i = 0; i < 16; i++) {
-          document.querySelectorAll(`.button`)[i].style.pointerEvents = `none`;
+          // disables clicking
+          for (let i = 0; i < 16; i++) {
+            document.querySelectorAll(`.button`)[
+              i
+            ].style.pointerEvents = `none`;
+          }
         }
       }
     });
@@ -111,6 +138,7 @@ document.querySelector(`.resetButton`).addEventListener(`click`, function () {
     document.querySelector(`.resetButton`).style.pointerEvents = `revert`;
   }, 1000);
 
+  // Resets the score
   document.querySelector(`.score`).innerHTML = randomNumberStorage.length;
 
   for (let i = 0; i < 16; i++) {
