397a21454ef5b8814f6fcfeb9ee328d0be72fe99
Giovanni Marino <giovanni-marino@lambdastudents.com>
2022-05-10 01:52:51 -0400

    fixed bugs and cleaned code

--- script.js
+++ script.js
@@ -1,77 +1,18 @@
 "use strict";
 
-// random number generator
-let randomNumber = 0;
-let randomNumberStorage = [];
+let randomNumberStorage = []; // Holds all the numbers
+let randomNumberStorageIndex = 0; // Used as the index for randomNumberStorage
+let userCanPressButtons = false; // Allows clicking or not
 
-let indexOfButtonToHighlight = 0;
-let sequenceRepeatCurrentIndex = 0; // used as the index for randomNumberStorage
-let userCanPressButtons = false;
-
-/* the first randomly generated flash + new randomly generated flashes generates a new number, turns the box purple, then transparent, then pushes the number to the arr */
-function newButton() {
-  sequenceRepeatCurrentIndex = 0;
-  // random number generator
-  let randomNumber = Math.ceil(Math.random() * 16);
-
-  randomNumberStorage.push(randomNumber);
-
-  //unrelated console logs
-  console.log(randomNumberStorage);
-  for (let index = 0; index < randomNumberStorage.length; index++) {
-    console.log(`highlight ${randomNumberStorage.length} times`);
-  }
-
-  // change score
-  document.querySelector(`.score`).innerHTML = randomNumberStorage.length - 1;
-
-  // change highscore
-  let score = Number(document.querySelector(`.score`).innerHTML);
-  let highScore = Number(document.querySelector(`.highScoreNumber`).innerHTML);
-
-  if (score > highScore) {
-    document.querySelector(`.highScoreNumber`).innerHTML =
-      document.querySelector(`.score`).innerHTML;
-  }
-  flashButton();
-}
-
-function flashButton() {
-  // Highlight each button from randomNumberStorage
-  let buttonClass2 = `.btn-${randomNumberStorage[indexOfButtonToHighlight]}`;
-
-  setTimeout(() => {
-    document.querySelector(buttonClass2).style.cssText = `
-    background-color: darkviolet;
-  `;
-
-    setTimeout(() => {
-      document.querySelector(buttonClass2).style.cssText = `
-    transition-delay: 0.05;
-    background-color: transparent;
-  `;
-    }, 250);
-
-    if (indexOfButtonToHighlight < randomNumberStorage.length - 1) {
-      indexOfButtonToHighlight++;
-
-      setTimeout(flashButton, 250);
-    } else {
-      userCanPressButtons = true;
-      indexOfButtonToHighlight = 0;
-    }
-  }, 250);
-}
-
-// highlighting a button on refresh
-window.onload = function () {
-  // selects a button (1-16)
+// Highlighting a button on refresh
+window.onload = () => {
+  // Selects a button (1-16)
   for (let i = 1; i < 17; i++) {
     // Check to see if the randomly generated number matches the number of the button clicked
-    document.querySelector(`.btn-${i}`).addEventListener(`click`, function () {
-      // if user clicks the RIGHT box: turn box green, then transparent
+    document.querySelector(`.btn-${i}`).addEventListener(`click`, () => {
+      // If user clicks the RIGHT box: turn box green, then transparent
       if (userCanPressButtons) {
-        if (i === randomNumberStorage[sequenceRepeatCurrentIndex]) {
+        if (i === randomNumberStorage[randomNumberStorageIndex]) {
           document.querySelector(`.btn-${i}`).style.cssText = `
             background-color: limegreen;
         `;
@@ -82,21 +23,20 @@ window.onload = function () {
             `;
           }, 50);
 
-          // makes a new button after changing box colors
-
-          if (sequenceRepeatCurrentIndex < randomNumberStorage.length - 1) {
-            sequenceRepeatCurrentIndex++;
+          // Measures if we finished the array of stored numbers, if not then we generate a new number
+          if (randomNumberStorageIndex < randomNumberStorage.length - 1) {
+            randomNumberStorageIndex++;
           } else {
+            userCanPressButtons = false;
             setTimeout(() => {
-              userCanPressButtons = false;
               newButton();
             }, 300);
           }
         }
 
-        // if user clicks the WRONG box: turn box red & disable clicking
+        // If user clicks the WRONG box: turn box red & disable clicking
         else {
-          // userCanPressButtons = false;
+          userCanPressButtons = false;
           document.querySelector(`.btn-${i}`).style.backgroundColor = `red`;
 
           // disables clicking
@@ -112,9 +52,69 @@ window.onload = function () {
   newButton();
 };
 
+// Generates a random number & handles score/highscore
+const newButton = () => {
+  // Resetting index
+  randomNumberStorageIndex = 0;
+
+  // Random number generator
+  let randomNumber = Math.ceil(Math.random() * 16);
+
+  // Pushing new box/number to end of the array
+  randomNumberStorage.push(randomNumber);
+
+  // Change score
+  document.querySelector(`.score`).innerHTML = randomNumberStorage.length - 1;
+
+  // Change highscore
+  let score = Number(document.querySelector(`.score`).innerHTML);
+  let highScore = Number(document.querySelector(`.highScoreNumber`).innerHTML);
+
+  if (score > highScore) {
+    document.querySelector(`.highScoreNumber`).innerHTML =
+      document.querySelector(`.score`).innerHTML;
+  }
+  flashButton();
+};
+
+// Flashes each button
+const flashButton = () => {
+  // Highlight each button from randomNumberStorage
+  let currentButton = `.btn-${randomNumberStorage[randomNumberStorageIndex]}`;
+  setTimeout(() => {
+    document.querySelector(currentButton).style.cssText = `
+    background-color: darkviolet;
+  `;
+    setTimeout(() => {
+      document.querySelector(currentButton).style.cssText = `
+    transition-delay: 0.05;
+    background-color: transparent;
+  `;
+    }, 250);
+
+    // Measures if we finished the array of stored numbers, if not then we flash next button
+    if (randomNumberStorageIndex < randomNumberStorage.length - 1) {
+      randomNumberStorageIndex++;
+      setTimeout(flashButton, 250);
+    } else {
+      userCanPressButtons = true;
+      randomNumberStorageIndex = 0;
+    }
+  }, 250);
+};
+
 // Reset Button
-document.querySelector(`.resetButton`).addEventListener(`click`, function () {
+document.querySelector(`.resetButton`).addEventListener(`click`, () => {
   randomNumberStorage = [];
+
+  userCanPressButtons = false;
+
+  for (let i = 1; i < 17; i++) {
+    document.querySelector(`.btn-${i}`).style.backgroundColor = `transparent`;
+  }
+
+  // Resets the score
+  document.querySelector(`.score`).innerHTML = randomNumberStorage.length;
 
   // Disables spam-clicking the reset button
   document.querySelector(`.resetButton`).style.pointerEvents = `none`;
@@ -122,31 +122,9 @@ document.querySelector(`.resetButton`).addEventListener(`click`, function () {
     document.querySelector(`.resetButton`).style.pointerEvents = `revert`;
   }, 1000);
 
-  for (let i = 0; i < 16; i++) {
-    document.querySelectorAll(`.button`)[i].style.pointerEvents = `none`;
-  }
-
-  setTimeout(() => {
-    for (let i = 0; i < 16; i++) {
-      document.querySelectorAll(`.button`)[i].style.pointerEvents = `revert`;
-    }
-  }, 10000);
-
-  // for (let i = 0; i < 16; i++) {
-  //   document.querySelectorAll(`.button`)[
-  //     i
-  //   ].style.pointerEvents = `revert`;
-  // }
-
-  // Resets the score
-  document.querySelector(`.score`).innerHTML = randomNumberStorage.length;
-
+  // Lets you click again. Clicking the wrong box turns off clicking so this is a safety measure
   for (let i = 0; i < 16; i++) {
     document.querySelectorAll(`.button`)[i].style.pointerEvents = `revert`;
-  }
-
-  for (let i = 1; i < 17; i++) {
-    document.querySelector(`.btn-${i}`).style.backgroundColor = `transparent`;
   }
 
   setTimeout(() => {
